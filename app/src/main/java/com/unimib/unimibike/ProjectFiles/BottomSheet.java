bfd6b89b8382c0c266960ae808942bac657b102e
conerns <andrei.taraboi@gmail.com>
2020-05-13 18:15:35 +0200

    Aggiunta lettura QRcode durante la fase di sblocco.

--- app/src/main/java/com/unimib/unimibike/ProjectFiles/BottomSheet.java
+++ app/src/main/java/com/unimib/unimibike/ProjectFiles/BottomSheet.java
@@ -1,61 +1,139 @@
 package com.unimib.unimibike.ProjectFiles;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.DialogFragment;
 
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.textfield.TextInputEditText;
 import com.unimib.unimibike.R;
+import com.unimib.unimibike.Util.QReader;
+
+import java.io.IOException;
 
 public class BottomSheet extends BottomSheetDialogFragment {
-    private TextInputEditText editComment;
+    private SurfaceView         surfaceView;
+    private CameraSource        cameraSource;
+    private BarcodeDetector     barcodeDetector;
+    private TextInputEditText   editComment;
+    private Button              mButton;
+    private View                w;
     @SuppressLint("ClickableViewAccessibility")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View w = inflater.inflate(R.layout.bottom_sheet_layout,container,false);
+        w = inflater.inflate(R.layout.bottom_sheet_layout,container,false);
         editComment = w.findViewById(R.id.bike_code_text);
-        Button prova =(Button) w.findViewById(R.id.button_unlock_bike);
-        prova.setOnClickListener(new View.OnClickListener() {
+        mButton     = w.findViewById(R.id.button_unlock_bike);
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("unlock-bike", "Funziona");
+                if(editComment.getText().length()!=0){
+                    //fai cose
+
+                }
             }
         });
         editComment.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                final int DRAWABLE_LEFT = 0;
-                final int DRAWABLE_TOP = 1;
+            public boolean onTouch(View view, MotionEvent event) {
+                view = w;
                 final int DRAWABLE_RIGHT = 2;
-                final int DRAWABLE_BOTTOM = 3;
 
                 if(event.getAction() == MotionEvent.ACTION_UP) {
                     if(event.getRawX() >= (editComment.getRight() - editComment.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
-                        /*BarcodeDetector detector =
-                                new BarcodeDetector.Builder(getActivity().getApplicationContext())
-                                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
-                                        .build();
-                        if(!detector.isOperational()){
-                            Log.d("BARCODE", "BARCODE NON FUNZIONA");
-                        }
-                        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-                        SparseArray<Barcode> barcodes = detector.detect(frame);*/
+                        editComment.setVisibility(View.INVISIBLE);
+                        mButton.setVisibility(View.INVISIBLE);
+                        surfaceView = view.findViewById(R.id.surface_area);
+                        surfaceView.setVisibility(View.VISIBLE);
+                        method_called();
                         return true;
                     }
                 }
                 return false;
             }
         });
+
         return w;
+    }
+
+    private void method_called() {
+        barcodeDetector = new BarcodeDetector.Builder(getActivity().getApplicationContext())
+                .setBarcodeFormats(Barcode.QR_CODE).build();
+
+        cameraSource = new CameraSource.Builder(getActivity().getApplicationContext(), barcodeDetector)
+                .setRequestedPreviewSize(640, 480).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                try {
+                    cameraSource.start(holder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCode = detections.getDetectedItems();
+                if (qrCode.size() != 0) {
+                    editComment.post(new Runnable() {
+                        @SuppressLint("SetTextI18n")
+                        @Override
+                        public void run() {
+                            editComment.setText(String.valueOf((qrCode.valueAt(0).displayValue)));
+                            cameraSource.stop();
+                            editComment.setVisibility(View.VISIBLE);
+                            surfaceView.setVisibility(View.GONE);
+                            mButton.setVisibility(View.VISIBLE);
+                        }
+                    });
+                }
+            }
+        });
+
     }
 }
