886a5452b556d2bbbd886dc135a1bd7df158e07c
Giuseppe Ziccardi <giuseppe.zick@outlook.it>
2020-05-18 17:26:09 +0200

    dovevo farlo per forza

--- app/src/main/java/com/unimib/unimibike/Util/Geolocation.java
+++ app/src/main/java/com/unimib/unimibike/Util/Geolocation.java
@@ -3,15 +3,25 @@ package com.unimib.unimibike.Util;
 import androidx.annotation.NonNull;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.content.IntentSender;
 import android.location.Location;
 import android.os.Looper;
+import android.util.Log;
+
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public class Geolocation{
@@ -19,6 +29,7 @@ public class Geolocation{
     private FusedLocationProviderClient mFusedLocationClient;
     private Activity activity;
     private GeolocationCallback geolocationCallback;
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
 
     public Geolocation(Activity activity, GeolocationCallback geolocationCallback){
         this.activity = activity;
@@ -27,6 +38,10 @@ public class Geolocation{
     }
     public Geolocation(){
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(new Activity());
+    }
+
+    public Geolocation(Activity activity){
+        this.activity = activity;
     }
 
     @SuppressLint("MissingPermission")
@@ -87,4 +102,45 @@ public class Geolocation{
         return Math.sqrt(distance);
     }
 
+
+    public void displayLocationSettingsRequest(final Context context) {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(context);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(activity, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+
+            }
+        });
+
+        task.addOnFailureListener(activity, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(activity,
+                                REQUEST_CHECK_SETTINGS);
+
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+    }
 }
