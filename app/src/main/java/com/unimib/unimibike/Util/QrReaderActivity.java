d03df974c316d36d5a7c28df557b44567db95a38
Giuseppe Ziccardi <giuseppe.zick@outlook.it>
2020-07-10 17:18:13 +0200

    qr code camera fixed resolution

--- app/src/main/java/com/unimib/unimibike/Util/QrReaderActivity.java
+++ app/src/main/java/com/unimib/unimibike/Util/QrReaderActivity.java
@@ -6,9 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,30 +14,29 @@ import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
-import com.unimib.unimibike.R;
+import com.unimib.unimibike.databinding.ActivityQrReaderBinding;
 
 import java.io.IOException;
 
 //TODO change with viewBinding
 
 public class QrReaderActivity extends AppCompatActivity {
-    private SurfaceView surfaceView;
-    private TextView textView;
+    private ActivityQrReaderBinding binding;
     private CameraSource cameraSource;
     private BarcodeDetector barcodeDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qr_reader);
-        surfaceView = (SurfaceView) findViewById(R.id.surface_area2);
-        surfaceView.setVisibility(View.VISIBLE);
-        textView = (TextView) findViewById(R.id.textViewForCamera);
+        binding = ActivityQrReaderBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.surfaceArea.setVisibility(View.VISIBLE);
         camerScan();
 
     }
 
     public void camerScan(){
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+
+        binding.surfaceArea.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
                 try {
@@ -51,9 +48,7 @@ public class QrReaderActivity extends AppCompatActivity {
 
 
             @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-            }
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {            }
 
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
@@ -66,8 +61,10 @@ public class QrReaderActivity extends AppCompatActivity {
         barcodeDetector = new BarcodeDetector.Builder(this)
                 .setBarcodeFormats(Barcode.QR_CODE).build();
 
-        cameraSource = new CameraSource.Builder(this, barcodeDetector).build();
-
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
         barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
             @Override
             public void release() {
@@ -78,7 +75,7 @@ public class QrReaderActivity extends AppCompatActivity {
             public void receiveDetections(Detector.Detections<Barcode> detections) {
                 final SparseArray<Barcode> qrCode = detections.getDetectedItems();
                 if (qrCode.size() != 0) {
-                    textView.post(new Runnable() {
+                    binding.textViewForCamera.post(new Runnable() {
                         @SuppressLint("SetTextI18n")
                         @Override
                         public void run() {
